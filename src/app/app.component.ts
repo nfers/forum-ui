564a513ccb44e2a4fca7c60198c16ffa8237bf3c
nfers <ncy.fers@gmail.com>
2021-03-09 17:56:05 -0300

    :fire: Configurações iniciais do projeto Forum On-line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "my-app",
+  selector: "forum-online",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
